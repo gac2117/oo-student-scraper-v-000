97f09083584cbad5c337cc43edc7025c63771572
Grace Choi <oiograceoio@gmail.com>
2019-03-06 20:12:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,19 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    unparsed = open(index_url)
-    page = Nokogiri::HTML(unparsed)
-    binding.pry
+    html = open(index_url)
+    page = Nokogiri::HTML(html)
+    students = Array.new
+    student_cards = page.css('div.student-card')
+    student_cards.each do |i|
+      student = {
+        name: i.css('.student-name').text
+        location: i.css('.student-location').text
+      }
   end
 
   def self.scrape_profile_page(profile_url)
