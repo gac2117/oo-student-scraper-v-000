ee3d9c9875d2be6f04c333d65fea082ed21eb8f5
Grace Choi <oiograceoio@gmail.com>
2019-03-06 20:11:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,13 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    unparsed = open(index_url)
+    page = Nokogiri::HTML(unparsed)
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
